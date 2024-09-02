32490b97a3724c59f43222c85d006eed685f1056
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-02 12:02:33 +0100

    Update __init__.py

--- Code/Quadruped/__init__.py
+++ Code/Quadruped/__init__.py
@@ -4,11 +4,11 @@ class Quadruped:
     def __init__(self,p,robot_id):
         self.robot_id =robot_id  #connect to simulation
         self.p=p
-        self.motors=[0 for i in range(3*4)]
-        self.neutral=[0 for i in range(3*4)]
-        
+        self.motors=[0 for i in range(3*4)] #positions in degrees
+        self.neutral=[0 for i in range(3*4)] #positions in degrees
+        self.start=self.getPos()
     def reset(self):
-        for joint_index in range(12):
+        for joint_index in range(12): 
             joint_info = self.p.getJointInfo(self.robot_id, joint_index)
             self.p.setJointMotorControl2(
                 bodyUniqueId=self.robot_id,
@@ -16,3 +16,17 @@ class Quadruped:
                 controlMode=self.p.POSITION_CONTROL,
                 targetPosition=maths.radians(self.neutral[joint_index])
             )
+    def setPositions(self,positions): #positions in degrees
+        for i in range(len(positions)):
+           self.motors[i]=positions[i]
+           self.p.setJointMotorControl2(
+                bodyUniqueId=self.robot_id,
+                jointIndex=i,
+                controlMode=self.p.POSITION_CONTROL,
+                targetPosition=maths.radians(maths.radians(self.motors[i]))
+            ) 
+    def getPos(self):
+        position, orientation = self.p.getBasePositionAndOrientation(self.robot_id)
+        # Extract the x, y, z position
+        x, y, z = position
+        return x,y,z
