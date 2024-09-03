88a561c9548013e43477e760edacdc2bf02fc13d
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-03 10:16:09 +0100

    code

--- Code/GA.py
+++ Code/GA.py
@@ -1,5 +1,5 @@
 path="C:/Users/dexte/Documents/GitHub/Quadruped/Quadruped_sim/urdf/"
-#path="/its/home/drs25/Documents/GitHub/Quadruped/Quadruped_sim/urdf/"
+path="/its/home/drs25/Documents/GitHub/Quadruped/Quadruped_sim/urdf/"
 import pybullet as p
 import pybullet_data
 import time
@@ -12,24 +12,29 @@ p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 print(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.81)
+
+def euclidean_distance(point1, point2):
+    return np.sqrt(np.sum((np.array(point1) - np.array(point2)) ** 2))
+
+
 def fitness(robot):
-    x,y,z=robot.getPos()
-    sx,sy,sz=robot.start
-    distance = maths.sqrt((x - sx)**2 + (y - sy)**2)
+    distance = (euclidean_distance(robot.getPos()[0:2],robot.start[0:2])*0.8 - euclidean_distance(robot.start_orientation,robot.getOrientation())*0.1)-robot.get_self_collision_count()*0.01
+    if distance<0: distance=0
     return distance
 
 def runTrial(agent,generations,delay=True):
     p.resetSimulation()
     p.setGravity(0, 0, -9.81)
     plane_id = p.loadURDF('plane.urdf')
-    initial_position = [0, 0, 0.2]  # x=1, y=2, z=0.5
+    initial_position = [0, 0, 0.3]  # x=1, y=2, z=0.5
     initial_orientation = p.getQuaternionFromEuler([0, 0, 0])  # No rotation (Euler angles to quaternion)
     flags = p.URDF_USE_SELF_COLLISION
-    robot_id = p.loadURDF(path+"Quadruped.urdf", initial_position, initial_orientation,flags=flags)
+    robot_id = p.loadURDF(path+"Quadruped_prestip.urdf", initial_position, initial_orientation,flags=flags)
     for i in range(100):
         p.stepSimulation()
         p.setTimeStep(1./24.)
     quad=Quadruped.Quadruped(p,robot_id)
+    quad.neutral=[-10,0,30,0,0,0,0,0,0,0,0,0]
     quad.reset()
     #GA
     for i in range(generations):
@@ -47,17 +52,17 @@ def runTrial(agent,generations,delay=True):
 
 def mutate(array,probability=0.2):
     flip_mask = np.random.rand(*array.shape) < probability
-    array[flip_mask] = np.where(array[flip_mask] == 30, 0, 30)
+    array[flip_mask] = np.random.choice([50,20,0,0,-20], size=np.count_nonzero(flip_mask))
     return array
 
 #initial
-population=np.random.choice([30, 0],(150,15,12)) #12 motors, 15 steps
+population=mutate(np.zeros((150,15,12)),probability=0.4)#np.random.choice([50, 20, 0,0,0,0,-20],(150,15,12)) #12 motors, 15 steps
 fitnesses=np.zeros((150,))
 generations=2000
 
 #get fitnesses
 for i in range(len(fitnesses)):
-    fitnesses[i]=runTrial(population[i],100,delay=False)
+    fitnesses[i]=runTrial(population[i],100,delay=0)
     print(i,"/",len(fitnesses), fitnesses[i])
 
 for gen in range(generations):
--- Code/test.py
+++ Code/test.py
@@ -5,27 +5,29 @@ import pybullet_data
 import time
 import math as maths
 import Quadruped
+import os
+import numpy as np
 
+clear = lambda: os.system('clear')
 # Initialize the PyBullet physics engine
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 plane_id = p.loadURDF('plane.urdf')
 flags = p.URDF_USE_SELF_COLLISION
-robot_id = p.loadURDF(path+"Quadruped.urdf",flags=flags)
+robot_id = p.loadURDF(path+"Quadruped_prestip.urdf",flags=flags)
 p.setGravity(0, 0, -9.81)
 
 quad=Quadruped.Quadruped(p,robot_id)
-quad.neutral=[80,1,1,1,0,0,0,0,0,0,0,0]
+quad.neutral=[-10,0,30,0,0,0,0,0,0,0,0,0]
 quad.reset()
 
+def euclidean_distance(point1, point2):
+    return np.sqrt(np.sum((np.array(point1) - np.array(point2)) ** 2))
+
+
 while 1:
-    inp=input(">").split()
-    i=inp[0]
-    deg=inp[1]
-    quad.neutral[int(i)]=int(deg)
-    quad.reset()
     for i in range(100):
         p.stepSimulation()
-        #print("")
-        # Optional sleep for real-time simulation
         time.sleep(1./240.)
+    clear()
+    print(quad.getOrientation(),quad.start_orientation,euclidean_distance(quad.start_orientation,quad.getOrientation()))
