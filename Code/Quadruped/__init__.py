e6bfb115c7646a644c2a4fa18f77108a025c8ce7
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-03 10:15:58 +0100

    Update __init__.py

--- Code/Quadruped/__init__.py
+++ Code/Quadruped/__init__.py
@@ -7,6 +7,7 @@ class Quadruped:
         self.motors=[0 for i in range(3*4)] #positions in degrees
         self.neutral=[0 for i in range(3*4)] #positions in degrees
         self.start=self.getPos()
+        self.start_orientation=self.getOrientation()
     def reset(self):
         for joint_index in range(12): 
             self.p.setJointMotorControl2(
@@ -29,3 +30,11 @@ class Quadruped:
         # Extract the x, y, z position
         x, y, z = position
         return x,y,z
+    def getOrientation(self):
+        position, orientation = self.p.getBasePositionAndOrientation(self.robot_id)
+        # Extract the x, y, z position
+        return  orientation
+    def get_self_collision_count(self):
+        # Get all contact points where the robot is in contact with itself
+        contact_points = self.p.getContactPoints(bodyA=self.robot_id, bodyB=self.robot_id)
+        return len(contact_points)
