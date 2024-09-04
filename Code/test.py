8de121178fdef26500449f475c38037b69879972
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-04 11:54:29 +0100

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -12,12 +12,14 @@ clear = lambda: os.system('clear')
 # Initialize the PyBullet physics engine
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-plane_id = p.loadURDF('plane.urdf')
+print(pybullet_data.getDataPath())
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+plane_id = p.loadURDF("plane.urdf")#/its/home/drs25/Documents/GitHub/Terrain_generator_3D/assets/test.urdf
 flags = p.URDF_USE_SELF_COLLISION
-robot_id = p.loadURDF(path+"Quadruped_prestip.urdf",flags=flags)
+robot_id = p.loadURDF(path+"Quadruped_prestip.urdf", [0, 0, 0.3], flags=flags)
 p.setGravity(0, 0, -9.81)
 
-quad=Quadruped.Quadruped(p,robot_id)
+quad=Quadruped.Quadruped(p,robot_id,plane_id)
 quad.neutral=[-10,0,30,0,0,0,0,0,0,0,0,0]
 quad.reset()
 
@@ -31,3 +33,8 @@ while 1:
         time.sleep(1./240.)
     clear()
     print(quad.getOrientation(),quad.start_orientation,euclidean_distance(quad.start_orientation,quad.getOrientation()))
+    contact=quad.getContact()
+    for i in range(len(contact)):
+        print("\t-- robot link",contact[i][3],"with force",contact[i][9],"Has fallen",quad.hasFallen())
+
+    print("Feet sensors",quad.getFeet())
