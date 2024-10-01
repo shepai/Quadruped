dee818e200ae66fba005b1536b9eed9d2f493f37
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-10-01 14:25:24 +0100

    Update testCPG.py

--- Code/testCPG.py
+++ Code/testCPG.py
@@ -13,32 +13,32 @@ class Network:
         np.fill_diagonal(self.weights, 0) #zero self weights
         self.Tau=1
         self.dt=0.01
-        self.b=0
-    def act(self,x): #activation function
-        return (1 + np.exp(-(self.b+x)))**-1
+        self.b=-4
     def sigma(self,x):
         return 1/(1+np.exp(-x))
     def forward(self,I=0):
         #TA=-self.A.T + np.dot(self.act(self.A.T),self.weights)+ I.T +self.bias #forward pass
-        O_j=self.act(self.A)
-        total_inputs=np.dot(self.weights, O_j)
-        self.A += (self.dt * (1/ self.Tau))
+        O=self.sigma(self.A+self.b)
+        self.A = self.A + (self.dt/self.Tau)*(-self.A + np.dot(self.weights,O)+I)
         return self.A
-
-model=Network(6)  
+    
+Neurons=6
+model=Network(Neurons)  
 arc=[deepcopy(model),deepcopy(model),deepcopy(model),deepcopy(model)]
 
 SAMPLE=300
-data=np.zeros((4,SAMPLE,6))
+data=np.zeros((4,SAMPLE,Neurons))
 last_Input=np.zeros_like(arc[0].A)
-for i in range(100):
+for i in range(SAMPLE):
     for j in range(4):
+        last_Input=np.random.random(Neurons)*10
         nodes=arc[j].forward(I=last_Input)
         lastInput=nodes.copy()
         data[j][i]=nodes.flatten()
 
 data=np.array(data)
-for j in range(4):
+print(data.shape)
+for j in range(Neurons):
     #for i in range(len(data[j].T)):
     #plt.plot(data[j].T[4],label="Output network"+str(j))
     plt.plot(data[0].T[j],label="Output neuron"+str(j))
