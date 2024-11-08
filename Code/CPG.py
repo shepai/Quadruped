da2460bb55c60f957f5c9806a25e11d4e457cab2
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-11-08 09:39:53 +0000

    train GA on updated arch

--- Code/CPG.py
+++ Code/CPG.py
@@ -1,8 +1,34 @@
 import numpy as np
 from copy import deepcopy
 from agent import *
-
 class Network:
+    def __init__(self, num, std=0.1):
+        self.num = num
+        self.std = std
+        self.A = np.random.uniform(0.1, 0.2, num) + np.random.normal(0, 0.05, num)  # Add noise
+        self.weights = np.random.normal(0, std, (num, num))  # Weight matrix
+        self.Tau = np.ones(num)  # Time constants
+        self.dt = 0.1
+        self.b = np.random.normal(0, std, num)  # Bias terms
+        self.O = self.sigma(self.A)
+        self.gains = np.random.normal(0, 5, (num, ))  # Gains for activation
+        self.reset()
+        self.bias_val=0
+
+    def reset(self):
+        self.A = np.random.uniform(0.1, 0.2, self.num) + np.random.normal(0, 0.05, self.num)
+        self.O = self.sigma(self.A)
+
+    def sigma(self, x):
+        return np.tanh(x) #1 / (1 + np.exp(-x))
+
+    def forward(self, I=0):
+        self.O = self.sigma(self.gains * (self.A + self.b))
+        total_inputs = np.dot(self.weights, self.O) + I
+        self.A += (self.dt / self.Tau) * (total_inputs - self.A)
+        
+        return self.O
+"""class Network:
     def __init__(self,num,std=5):
         self.A=np.zeros((num))
         self.weights=np.random.normal(0,std,(num,num))
@@ -11,19 +37,19 @@ class Network:
         self.dt=0.01
         self.b=0
     def sigma(self,x):
-        return 1/(1+np.exp(-x))
+        return np.tanh(x)#1/(1+np.exp(-x))
     def forward(self,I=0):
         #TA=-self.A.T + np.dot(self.act(self.A.T),self.weights)+ I.T +self.bias #forward pass
         O=self.sigma(self.A+self.b)
         self.A = self.A + (self.dt/self.Tau)*(-self.A + np.dot(self.weights,O)+I)
-        return self.A
+        return self.A"""
     
 class CPG(agent):
     def __init__(self,num_neurons):
         agent.__init__(self)
         self.cpg=Network(num_neurons)
         self.num_neurons=num_neurons
-        self.geno=np.concatenate([self.cpg.weights.flatten(),[self.cpg.b],[self.cpg.Tau]])
+        self.geno=np.concatenate([self.cpg.weights.flatten(),self.cpg.b.flatten(),self.cpg.Tau.flatten()])
         self.set_genotype(self.geno)
         self.populateBody()
     def populateBody(self):
@@ -48,17 +74,18 @@ class CPG(agent):
             positions.append(Inputs[0]) #select neuron outputs as proportional to motor
             positions.append(Inputs[1])
             positions.append(Inputs[2])
+        
         return np.array(positions)
     def set_genotype(self, values):
         self.cpg.weights=values[0:len(self.cpg.weights.flatten())].reshape(self.cpg.weights.shape)
-        self.cpg.b=values[len(self.cpg.weights.flatten())]
-        self.cpg.b = -4 if self.cpg.b < -4 else self.cpg.b #cap bias
-        self.cpg.b = 4 if self.cpg.b > 4 else self.cpg.b #cap bias
-        self.cpg.Tau=values[len(self.cpg.weights.flatten())+1]
+        self.cpg.b=values[len(self.cpg.weights.flatten()):len(self.cpg.weights.flatten())+len(self.cpg.b.flatten())]
+        self.cpg.b[self.cpg.b<-4] = -4#cap bias
+        self.cpg.b[self.cpg.b>4] = 4 #cap bias
+        self.cpg.Tau=values[len(self.cpg.weights.flatten())+len(self.cpg.b.flatten()):len(self.cpg.weights.flatten())+len(self.cpg.b.flatten())+len(self.cpg.Tau)].reshape(self.cpg.Tau.shape)
         self.cpg.weights[self.cpg.weights>16]=16 #cap weights
         self.cpg.weights[self.cpg.weights<-16]=-16 #cap weights
         self.populateBody()
-        return super().set_genotype(np.concatenate([self.cpg.weights.flatten(),[self.cpg.b],[self.cpg.Tau]]))
+        return super().set_genotype(np.concatenate([self.cpg.weights.flatten(),self.cpg.b.flatten(),self.cpg.Tau.flatten()]))
     
 
 if __name__ == "__main__":
