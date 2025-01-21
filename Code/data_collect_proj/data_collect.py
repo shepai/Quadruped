ed9333466175d9b5ff853b680f5230836a6ff61a
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-01-21 11:44:24 +0000

    updates

---
+++ Code/data_collect_proj/data_collect.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#import simulator 
+from environment import *
+from CPG import *
+import numpy as np
+
+
+#create dataset writer class
+class dataset:
+    def __init__(self):
+        pass
+
+#reset trial
+#launch trial
+#random moves
+#save moves and what happened 
