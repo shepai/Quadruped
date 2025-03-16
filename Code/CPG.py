4847ce6074950f4e0957e8496d659a0c68cda702
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-03-16 20:33:28 +0200

    Update CPG.py

--- Code/CPG.py
+++ Code/CPG.py
@@ -161,7 +161,7 @@ class CTRNNQuadruped:
     def get_positions(self,inputs,motors=None):
         degrees=np.degrees(self.step(imu_feedback=0, velocity_feedback=0))/1.5
         degrees=np.clip(degrees,0,180)
-        degrees[[2,5,8,11]]=180-degrees[[1,4,7,10]]
+        degrees[[2,5,8,11]]=degrees[[1,4,7,10]]
         degrees[3:9]=-degrees[3:9] #try running this 
         return degrees
     def step(self, imu_feedback, velocity_feedback):
