0decf19234d952f57fcfb968d5968d2ff024ed60
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-03-17 18:12:45 +0200

    ga play and run

--- Code/GAs/Play_GA.py
+++ Code/GAs/Play_GA.py
@@ -7,39 +7,19 @@ datapath="C:/Users/dexte/Documents/GitHub/Quadruped/"
 from environment import *
 from CPG import *
 import pickle
-with open(datapath+'/models/genotypes_4.pkl', 'rb') as f:
+with open(datapath+'/models/genotypes_dt0.2=.pkl', 'rb') as f:
     population = pickle.load(f)
-fitnesses=np.load(datapath+"/models/fitnesses_5.npy")
+fitnesses=np.load(datapath+"/models/fitnesses_dt0.2.npy")
 def fitness_(robot,history={}): 
     fitness=0
     #look at behaviour over time
     if len(history.get('motors',[]))>0:
-        #calculate the phase length of the hip
-        """oscillations = np.diff(np.array(history['motors'])[:,3])
-        zero_crossings = np.where(np.diff(np.sign(oscillations)) != 0)[0] + 1  # +1 to correct index shift
-        diff=np.diff(zero_crossings)
-        if type(np.diff(zero_crossings))==type([]) or type(np.diff(zero_crossings))==type(np.array([])): 
-            if len(np.diff(zero_crossings))>0:diff=np.average(diff)
-            else: diff=0
-        fitness+=diff/10 #more phase is betters"""
-        #distance over time
         distances=np.array(history['positions'])-np.array([robot.start])#euclidean_distance(np.array(history['positions']),np.array([robot.start]))
         distancesX=distances[-1][0]
         distancesY=distances[-1][1]
         distancesZ=distances[-1][2]
         fitness+=distancesX - (distancesY+distancesZ)/10 #np.sum(distances)
-        #orientationo over time#
-        """stability_penalty = np.mean(np.linalg.norm(np.array(history['orientations']) - np.array(robot.start_orientation), axis=1))
-        jerkiness_penalty = np.sum(np.linalg.norm(np.diff(np.array(history['orientations']), axis=0), axis=1))
-        fitness -= 0.01 * stability_penalty + 0.001 * jerkiness_penalty"""
-    else: #basic fitness
-        distance = euclidean_distance(np.array([robot.start]),np.array([robot.getPos()]))
-        orientation_penalty = np.linalg.norm(np.array(robot.getOrientation()) - np.array(robot.start_orientation)) 
-        distance *= np.exp(-0.1 * orientation_penalty)  # Penalize unstable rotations
-        direction_vector = np.array(robot.getPos()[0:2]) - np.array(robot.start[0:2])
-        goal_direction = np.array([1, 0])  # Example: moving in +x direction
-        direction_reward = np.dot(direction_vector, goal_direction) / (np.linalg.norm(direction_vector) + 1e-6)
-        distance *= (1 + direction_reward)
+
     if robot.hasFallen(): fitness=0
     if type(fitness)!=type(0): 
         try:
@@ -67,10 +47,9 @@ for i in range(len(fitnesses)): #
         index=i
 
 env.close()"""
-print(index)
-index=41
-env=environment(True,1,datapath+"/assets/videos/example_5_dt_higher.mp4")
+index=np.argmax(fitnesses)
+env=environment(True,1,datapath+"/assets/videos/example_dt0.2.mp4")
 population[index].dt=1
 fit,mot=env.runTrial(population[index],100,delay=1,fitness=fitness_)
 env.stop()
-np.savez(datapath+"/Code/GAs/motors_5",mot,allow_pickle=True)
+np.savez(datapath+"/Code/GAs/motors_dt0.2",mot,allow_pickle=True)
