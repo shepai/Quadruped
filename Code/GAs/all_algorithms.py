6b3d70a6853b031decc227468e6325b38f25edd5
shepai <dextershepherd@hotmail.co.uk>
2025-06-03 10:07:37 +0000

    environment update

--- Code/GAs/all_algorithms.py
+++ Code/GAs/all_algorithms.py
@@ -21,15 +21,15 @@ def RUN_diff(dt=0.1,sho=0,trial=0,generations=300,fit=fitness_,fric=0.5):
     mutation_rate=0.2
     ga=Differential(population_size, generations, mutation_rate,0.2)
     ga.initialize_population(CTRNNQuadruped, [4,3,0.1,0])
-    ga.evolve(env, fit, 20,outputs=1)
+    history,fitnesses=ga.evolve(env, fit, 20,outputs=1)
     t_start=time.time()
     #get fitnesses
     with open(datapath+'/models/genotypes_dt'+str(dt)+"_"+str(trial)+str(fric)+'.pkl', 'wb') as f:
-        pickle.dump(population, f)
+        pickle.dump(ga.pop, f)
     np.save(datapath+'/models/fitnesses_dt'+str(dt)+"_"+str(trial)+str(fric),fitnesses)
     np.save(datapath+'/models/history_dt'+str(dt)+"_"+str(trial)+str(fric),history)
 
-    env.runTrial(population[np.where(fitnesses==np.max(fitnesses))[0][0]],150,fitness=fit)
+    env.runTrial(ga.pop[np.where(fitnesses==np.max(fitnesses))[0][0]],150,fitness=fit)
     print("top fitness:",np.max(fitnesses))
     p.disconnect()
 
@@ -44,15 +44,15 @@ def RUN_microbial(dt=0.1,sho=0,trial=0,generations=300,fit=fitness_,fric=0.5):
     mutation_rate=0.2
     ga=Microbial_GA(population_size, generations, 0.2)
     ga.initialize_population(CTRNNQuadruped, [4,3,0.1,0])
-    ga.evolve(env, fit, 20,outputs=1)
+    history,fitnesses=ga.evolve(env, fit, 20,outputs=1)
     t_start=time.time()
     #get fitnesses
     with open(datapath+'/models/genotypes_dt'+str(dt)+"_"+str(trial)+str(fric)+'.pkl', 'wb') as f:
-        pickle.dump(population, f)
+        pickle.dump(ga.pop, f)
     np.save(datapath+'/models/fitnesses_dt'+str(dt)+"_"+str(trial)+str(fric),fitnesses)
     np.save(datapath+'/models/history_dt'+str(dt)+"_"+str(trial)+str(fric),history)
 
-    env.runTrial(population[np.where(fitnesses==np.max(fitnesses))[0][0]],150,fitness=fit)
+    env.runTrial(ga.pop[np.where(fitnesses==np.max(fitnesses))[0][0]],150,fitness=fit)
     print("top fitness:",np.max(fitnesses))
     p.disconnect()
 
@@ -66,29 +66,33 @@ def RUN_hillclimber(dt=0.1,sho=0,trial=0,generations=300,fit=fitness_,fric=0.5):
     mutation_rate=0.2
     ga=Hillclimbers(population_size, generations, 0.2)
     ga.initialize_population(CTRNNQuadruped, [4,3,0.1,0])
-    ga.evolve(env, fit, 20,outputs=1)
+    history,fitnesses=ga.evolve(env, fit, 20,outputs=1)
     t_start=time.time()
     #get fitnesses
     with open(datapath+'/models/genotypes_dt'+str(dt)+"_"+str(trial)+str(fric)+'.pkl', 'wb') as f:
-        pickle.dump(population, f)
+        pickle.dump(ga.pop, f)
     np.save(datapath+'/models/fitnesses_dt'+str(dt)+"_"+str(trial)+str(fric),fitnesses)
     np.save(datapath+'/models/history_dt'+str(dt)+"_"+str(trial)+str(fric),history)
 
-    env.runTrial(population[np.where(fitnesses==np.max(fitnesses))[0][0]],150,fitness=fit)
+    env.runTrial(ga.pop[np.where(fitnesses==np.max(fitnesses))[0][0]],150,fitness=fit)
     print("top fitness:",np.max(fitnesses))
     p.disconnect()
 
     t_passed=time.time()-t_start
     print("********************************\n\n\n\nTIME IT TOOK:",t_passed/(60*60),"Hours")
 if __name__=="__main__":
-    RUN_diff(dt=0.1,sho=0,generations=300,trial="differential_"+str(0)+"_friction",fit=F3,fric=0.1)
-    """DT=0.1
-    c=0
+    for i in range(20):
+        #RUN_microbial(dt=0.1,sho=0,generations=300,trial="LongMicrobial_"+str(i)+"_friction",fit=F3,fric=0.5)
+        RUN_hillclimber(dt=0.1,sho=0,generations=300,trial="Hillclimber_F1_"+str(i)+"_friction",fit=F1,fric=0.5)
+        RUN_hillclimber(dt=0.1,sho=0,generations=300,trial="Hillclimber_F2_"+str(i)+"_friction",fit=F2,fric=0.5)
+        RUN_hillclimber(dt=0.1,sho=0,generations=300,trial="Hillclimber_F3_"+str(i)+"_friction",fit=F3,fric=0.5)
+        #RUN_diff(dt=0.1,sho=0,generations=300,trial="Differential_"+str(i)+"_friction",fit=F3,fric=0.5)
+
+    """c=0
     for i in range(0,40):
         for j in np.arange(0,1,0.05):
             os.system('cls' if os.name == 'nt' else 'clear')
             calc=len(range(0,40))*len( np.arange(0,1,0.05))
             print(i,j,c/calc *100,"%")
-            RUN(dt=0.1,sho=0,trial="6_neurons_"+str(i)+"_friction",fit=F3,fric=j)
+            RUN_hillclimber(dt=0.1,sho=0,trial="hill_climber_"+str(i)+"_friction",fit=F3,fric=j)
             c+=1"""
-
