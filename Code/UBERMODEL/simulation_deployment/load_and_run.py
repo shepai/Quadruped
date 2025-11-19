09a1f60cfa8e7bb8d211d79292f30b871a1ef240
shepai <dextershepherd@hotmail.co.uk>
2025-11-19 13:52:44 +0000

    Update load_and_run.py

--- Code/UBERMODEL/simulation_deployment/load_and_run.py
+++ Code/UBERMODEL/simulation_deployment/load_and_run.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 import sys
-
+sys.path.insert(1, '/its/home/drs25/Documents/GitHub/Quadruped/Code')
+sys.path.insert(1, 'C:/Users/dexte/Documents/GitHub/Quadruped/Code')
+sys.path.insert(1, '/its/home/drs25/Quadruped/Code')
+import environment
 
 ###################
 # Transformer
@@ -16,13 +19,24 @@ import sys
 
 
 #select model that is used
-
+model=
 #create environment 
 vector=[_,_,_]
-friction=
+friction=0.5
+env=environment(0,friction=friction)
+timesteps=100
+dt=0.1
 #loop through time steps
-#get observation
-#get model output
+for i in np.arange(0,timesteps,dt):
+    #get observation
+    feet=env.quad.getFeet()
+    positions=env.quad.getPositions()
+    data=np.concatenate(positions,feet,[friction])
+    motors=model(data)
+    env.quad.setPositions(motors)#get model output
+    for k in range(10): #update simulation
+        p.stepSimulation()
+        p.setTimeStep(1./240.)
 #directly set positions of robot to model outputs
 
 
