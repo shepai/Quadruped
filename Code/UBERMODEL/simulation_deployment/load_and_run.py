74fe355db6aad6b19ae8eae34f176582917f9a96
shepai <dextershepherd@hotmail.co.uk>
2025-11-19 12:53:19 +0000

    structue code

---
+++ Code/UBERMODEL/simulation_deployment/load_and_run.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('TkAgg')
+import sys
+
+
+###################
+# Transformer
+###################
+
+
+###################
+# LSTM
+###################
+
+
+#select model that is used
+
+#create environment 
+vector=[_,_,_]
+friction=
+#loop through time steps
+#get observation
+#get model output
+#directly set positions of robot to model outputs
+
+
+#save trajectory 
+#show trajectory for sake of debugging 
+
+
