ea014a4b1d80105257890496d5ec19d626ae86dd
shepai <dextershepherd@hotmail.co.uk>
2025-11-19 15:46:41 +0000

    keras model and sim

--- Code/UBERMODEL/simulation_deployment/load_and_run.py
+++ Code/UBERMODEL/simulation_deployment/load_and_run.py
@@ -6,40 +6,57 @@ import sys
 sys.path.insert(1, '/its/home/drs25/Documents/GitHub/Quadruped/Code')
 sys.path.insert(1, 'C:/Users/dexte/Documents/GitHub/Quadruped/Code')
 sys.path.insert(1, '/its/home/drs25/Quadruped/Code')
-import environment
+from environment import *
 
 ###################
 # Transformer
 ###################
-
-
+"""from Code.UBERMODEL.Transformer.trainTransformer import MotorTransformer, generate_noisy_sine_dataset
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = MotorTransformer(input_dim=V1,output_dim=V2,T=T).to(device)
+model.load_state_dict(torch.load("/its/home/drs25/Quadruped/Code/UBERMODEL/models/gait_transformer.pth"))
+model.eval()
+"""
 ###################
 # LSTM
 ###################
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+model=load_model("/its/home/drs25/Quadruped/Code/UBERMODEL/models/gait_model.keras")
 
 
-#select model that is used
-model=
 #create environment 
-vector=[_,_,_]
+vector=[0.01,0,0]
 friction=0.5
 env=environment(0,friction=friction)
+env.reset()
 timesteps=100
 dt=0.1
 #loop through time steps
+rolling=[]
+traj=[]
 for i in np.arange(0,timesteps,dt):
     #get observation
     feet=env.quad.getFeet()
     positions=env.quad.getPositions()
-    data=np.concatenate(positions,feet,[friction])
-    motors=model(data)
-    env.quad.setPositions(motors)#get model output
+    data=np.concatenate([positions,vector,feet,[friction]])
+    rolling.append(data)
+    if len(rolling)>100:
+        rolling.pop(0)
+    data=np.array(rolling)
+    if len(data.shape)<3:
+        data=data.reshape((1,len(data),20))
+    motors=model.predict(data)
+    #motors=model(data.to(device))
+    env.quad.setPositions(motors[-1][-1])#get model output
     for k in range(10): #update simulation
         p.stepSimulation()
         p.setTimeStep(1./240.)
+    traj.append(env.quad.getPos())
 #directly set positions of robot to model outputs
-
-
+traj=np.array(traj)
+plt.plot(traj[:,0],traj[:,1])
+plt.show()
 #save trajectory 
 #show trajectory for sake of debugging 
 
