31e7a602585d81da58f8dcd5f943f0d72bc571cb
shepai <dextershepherd@hotmail.co.uk>
2025-11-21 16:55:40 +0000

    cool

--- Code/UBERMODEL/simulation_deployment/load_and_run.py
+++ Code/UBERMODEL/simulation_deployment/load_and_run.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import matplotlib
-matplotlib.use('TkAgg')
+#matplotlib.use('TkAgg')
 import sys
 sys.path.insert(1, '/its/home/drs25/Documents/GitHub/Quadruped/Code')
 sys.path.insert(1, 'C:/Users/dexte/Documents/GitHub/Quadruped/Code')
@@ -11,22 +11,26 @@ from environment import *
 ###################
 # Transformer
 ###################
-"""from Code.UBERMODEL.Transformer.trainTransformer import MotorTransformer, generate_noisy_sine_dataset
+"""from UBERMODEL.Transformer.trainTransformer import MotorTransformer, generate_noisy_sine_dataset
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = MotorTransformer(input_dim=V1,output_dim=V2,T=T).to(device)
+model = MotorTransformer(input_dim=20,output_dim=12,T=200).to(device)
 model.load_state_dict(torch.load("/its/home/drs25/Quadruped/Code/UBERMODEL/models/gait_transformer.pth"))
 model.eval()
 """
 ###################
 # LSTM
 ###################
-from tensorflow.keras.models import load_model
+"""from tensorflow.keras.models import load_model
 import tensorflow as tf
-model=load_model("/its/home/drs25/Quadruped/Code/UBERMODEL/models/gait_model.keras")
-
+model=load_model("/its/home/drs25/Quadruped/Code/UBERMODEL/models/gait_model.keras")"""
+from UBERMODEL.LSTM.PytorchLSTMTrain import LSTMModel
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = LSTMModel(input_dim=20, output_dim=12).to(device)
+model.load_state_dict(torch.load("/its/home/drs25/Quadruped/Code/UBERMODEL/models/lstm_gait_autoregressiveDeltas.pth"))
+model.eval()
 
 #create environment 
-vector=[0.01,0,0]
+vector=[0.1,0,0]
 friction=0.5
 env=environment(0,friction=friction)
 env.reset()
@@ -35,20 +39,27 @@ dt=0.1
 #loop through time steps
 rolling=[]
 traj=[]
+motors_=[]
+X=np.load("/its/home/drs25/Quadruped/Code/UBERMODEL/models/X_DATA.npy")[:100]
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 for i in np.arange(0,timesteps,dt):
     #get observation
     feet=env.quad.getFeet()
     positions=env.quad.getPositions()
     data=np.concatenate([positions,vector,feet,[friction]])
+    data=(data-np.min(X))/(np.max(X)-np.min(X))
     rolling.append(data)
-    if len(rolling)>100:
+    if len(rolling)>10:
         rolling.pop(0)
     data=np.array(rolling)
     if len(data.shape)<3:
         data=data.reshape((1,len(data),20))
-    motors=model.predict(data)
-    #motors=model(data.to(device))
-    env.quad.setPositions(motors[-1][-1])#get model output
+    
+    motors=model(torch.tensor(data,dtype=torch.float32).to(device))
+    #motors=model(torch.tensor(data,  dtype=torch.float32).to(device)).cpu().detach().numpy()
+    motors=motors[-1][-1].cpu().detach().numpy()+rolling[-1][:12]
+    env.quad.setPositions(motors)#get model output
+    motors_.append(motors) #.cpu().detach().numpy()[-1][-1]
     for k in range(10): #update simulation
         p.stepSimulation()
         p.setTimeStep(1./240.)
@@ -57,6 +68,8 @@ for i in np.arange(0,timesteps,dt):
 traj=np.array(traj)
 plt.plot(traj[:,0],traj[:,1])
 plt.show()
+plt.plot(motors_)
+plt.savefig("/its/home/drs25/Quadruped/assets/plots/traj.pdf")
 #save trajectory 
 #show trajectory for sake of debugging 
 
